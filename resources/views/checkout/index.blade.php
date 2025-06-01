e1e03247f9fe5219f29568e4d10207311fce48f1
Muhammad Rafli Adyatma <rafliadyatma17@gmail.com>
2025-06-01 21:51:50 +0700

    update_lagi

--- resources/views/checkout/index.blade.php
+++ resources/views/checkout/index.blade.php
@@ -17,34 +17,56 @@
                         <div class="col-md-6">
                             <img src="{{ url('assets') }}/{{ $barang->gambar }}" class="img-fluid" width="100%" alt="">
                         </div>
-                        <div class="col-md-6">
+                        <div class="col-md-6 mt-5">
                             <h2 class="card-title">{{ $barang->nama_barang }}</h2>
                             <table class="table">
-                                <thead>
-                                    <tr>
-                                        <th></th>
-                                        <th></th>
-                                        <th></th>
-                                    </tr>
-                                </thead>
                                 <tbody>
                                     <tr>
                                         <td scope="row"></td>
-                                        <td></td>
-                                        <td></td>
+                                        <td>Harga</td>
+                                        <td>:</td>
+                                        <td>Rp. {{number_format($barang->harga_barang)}}</td>
                                     </tr>
                                     <tr>
                                         <td scope="row"></td>
-                                        <td></td>
-                                        <td></td>
+                                        <td>Stok</td>
+                                        <td>:</td>
+                                        <td>{{$barang->stok_barang}}</td>
                                     </tr>
+                                    <tr>
+                                        <td scope="row"></td>
+                                        <td>Keterangan</td>
+                                        <td>:</td>
+                                        <td>{{$barang->keterangan}}</td>
+                                    </tr>
+                                    <form action="" method="post">
+                                        @csrf
+                                        <tr>
+                                            <td scope="row"></td>
+                                            <td>Jumlah Pesanan</td>
+                                            <td>:</td>
+                                            <td>
+                                                <input type="number" name="jumlah" class="form-control" value="1" min="1" max="{{$barang->stok_barang}}" required>
+                                                <input type="hidden" name="kode_barang" value="{{$barang->kode_barang}}">
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td></td>
+                                            <td></td>
+                                            <td></td>
+                                            <td colspan="3">
+                                                <button type="submit" class="btn btn-primary">Checkout</button>                                            </td>
+                                            </td>
+                                        </tr>
+                                    </form>
                                 </tbody>
                             </table>
-
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+    <a href="{{ url('home') }}" class="btn btn-danger">← Kembali</a>
 </div>
 @endsection
