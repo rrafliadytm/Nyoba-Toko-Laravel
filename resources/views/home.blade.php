2a3a36f0cef5ae028ef70a5a8ae383e81a9086a0
Muhammad Rafli Adyatma <rafliadyatma17@gmail.com>
2025-06-01 19:25:56 +0700

    update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-12 mb-5">
+        <div class="col-md-12 mb-4">
         </div>
         @foreach ($barang as $barang)
         <div class="col-md-4">
@@ -11,7 +11,7 @@
                 <img src="{{ url('assets') }}/{{ $barang->gambar }}" class="card-img-top" alt="...">
                 <div class="card-body">
                     <h5 class="card-title">{{ $barang->nama_barang }}</h5>
-                    <p class="card-text">{{$barang->keterangan}}</p>
+                    <p class="card-text"><strong>Deskripsi: </strong>{{$barang->keterangan}}</p>
                     <hr>
                     <p class="card-text mb-3"><strong>Harga: </strong> Rp. {{number_format($barang->harga_barang)}}</p>
                     <p class="card-text"><strong>Stok: </strong> {{ $barang->stok_barang }}</p>
