00cbd1358b9846bbba28af4e9febda23b2801ad7
rrafliadytm <rafliadyatma17@gmail.com>
2025-06-02 18:42:10 +0700

    update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,6 +4,7 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-12 mb-4">
+            <h1>Kamu mau kemana? Silahkan pilih maskapai anda!</h1>
         </div>
         @foreach ($barang as $barang)
         <div class="col-md-4">
