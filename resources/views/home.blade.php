fb950f5ac8c0b7f1c2edcfe87b701558657ec21b
rrafliadytm <rafliadyatma17@gmail.com>
2025-06-08 08:58:43 +0700

    update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
         <div class="carousel-inner">
             <div class="carousel-item active">
-                <img class="d-block w-100" src="/assets/celana.jpeg" alt="First slide">
+                <img class="d-block w-100" src="/assets/celana.jpeg" alt="First slide"> 
             </div>
             <div class="carousel-item">
                 <img class="d-block w-100" src="/assets/baju.jpeg" alt="Second slide">
